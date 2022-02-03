ac701a5689a479ea123574de44254e95a67c6a79
Starinin Andrey <starininandrey@gmail.com>
2022-02-03 21:21:42 +0300

    Init

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WpfApp3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clear_OnClick(object sender, RoutedEventArgs e)
+        {
+            Input_Name.Clear();
+            Input_Age.Clear();
+        }
+
+        private void Button_Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            var msg = $"{Input_Name.Text}, {Input_Age.Text}";
+            MessageBox.Show(msg);
+        }
+    }
+}
